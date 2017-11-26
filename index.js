b95b0de113f41e53dea9b89b4a861b076dfc4f54
kirmayr costa <kirmayr.costa@concrete.com.br>
2017-11-26 00:53:51 -0200

    :construction: Inserindo estrutura inicial da api

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const Promise = require('bluebird')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.json({api: 'Hello Human, ready to play? API works!'})
+})
+
+app.listen(3000, function () {
+  console.log('API READY')
+})
